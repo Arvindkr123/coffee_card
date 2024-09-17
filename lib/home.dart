90b752a83ba3e92636c6c59c82b94d603f05aa56
arvind kumar <thakurarvindkr10@gmail.com>
2024-09-17 09:35:01 +0530

    Image functionality completed

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:coffee_card/coffe_prefs.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -7,10 +8,30 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('My Coffee Id'),
-          backgroundColor: Colors.brown[100],
+          title: const Text(
+            'My Coffee Id',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          backgroundColor: Colors.brown[700],
           centerTitle: true,
         ),
-        body: const Text('hello Guru'));
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              color: Colors.brown[200],
+              padding: const EdgeInsets.all(20),
+              child: const Text('How I like my coffee...'),
+            ),
+            Container(
+              color: Colors.brown[100],
+              padding: const EdgeInsets.all(20),
+              child: const CoffeePrefs(),
+            ),
+          ],
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,32 +4,3 @@ import "package:coffee_card/home.dart";
 void main() {
   runApp(const MaterialApp(home: Home()));
 }
-
-class Sandbox extends StatelessWidget {
-  const Sandbox({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sandbox',
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
-        backgroundColor: Colors.grey,
-        centerTitle: true,
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-              color: Colors.brown[200],
-              padding: const EdgeInsets.all(20),
-              child: const Text('How I like my coffee..')),
-          Container(
-              color: Colors.brown[100],
-              padding: const EdgeInsets.all(20),
-              child: const Text('coffee prefes.'))
-        ],
-      ),
-    );
-  }
-}
